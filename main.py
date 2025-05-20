42f4902ba42b983664bdee0afce27aff0bd079a4
kajahl <kacperjahl37@gmail.com>
2025-05-20 16:01:28 +0200

    Dodano parametryzacje num pages

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from scraping import scrap_data_to_excel
+import argparse
+
 if __name__ == "__main__":
-    scrap_data_to_excel(num_pages=1)
+    parser = argparse.ArgumentParser(description="Scrap data from otomoto.pl")
+    parser.add_argument("-n", "--num-pages", type=int, default=1, help="Liczba stron do pobrania")
+    args = parser.parse_args()
+    scrap_data_to_excel(num_pages=args.num_pages)
